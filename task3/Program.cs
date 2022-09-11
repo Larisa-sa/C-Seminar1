0301ec78a6130d26096363b047f82da28be2c4f8
Larisa <larisamash01@gmail.com>
2022-09-11 22:17:08 +0300

    Задача про целочисленное деление (показать последнюю цифру в трехзначном числе

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и на выходе показывает последнюю цифру этого числа
+Console.Write("Введите трехзначное число : ");          
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    Console.WriteLine(number % 10);
+
+}
+else
+{
+    Console.WriteLine("Введено неверное значение");
+}
