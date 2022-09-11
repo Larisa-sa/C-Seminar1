f5460e0e001ddbd6932c75b4b6dacb2b1446a519
Larisa <larisamash01@gmail.com>
2022-09-11 18:15:41 +0300

    Изменила решение задачи про дни неделии

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,38 +4,20 @@
 
 Console.Write("Введите номер дня недели: ");
 string day = Console.ReadLine();   
-if (day == "1")
-{
-        Console.Write("Понедельник");
 
-}
-else if (day == "2")
-{
-            Console.Write("Вторник");
+if (day == "1") Console.Write("Понедельник");
 
-}
-else if (day == "3")
-{
-            Console.Write("Среда");
+else if (day == "2") Console.WriteLine("Вторник");
 
-}
-else if (day == "4")
-{
-            Console.Write("Четверг");
+else if (day == "3") Console.WriteLine("Среда");
 
-}
-else if (day == "5")
-{
-            Console.Write("Пятница");
+else if (day == "4") Console.WriteLine("Четверг");
 
-}
-else if (day == "6")
-{
-            Console.Write("Суббота");
+else if (day == "5") Console.WriteLine("Пятница");
 
-}
-else if (day == "7")
-{
-            Console.Write("Воскресенье");
+else if (day == "6") Console.WriteLine("Суббота");
 
-}
+else if (day == "7") Console.WriteLine("Воскресенье");
+
+else 
+Console.WriteLine("Нет такого");
